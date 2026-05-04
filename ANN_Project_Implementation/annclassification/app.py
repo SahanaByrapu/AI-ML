fd6b2f8b42919c843a28a057f849a97e2367b050
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-05-04 12:14:25 -0700

    Refactor model loading and improve code structure
    Updated model loading to use MODEL_PATH constant and improved code formatting.

--- ANN_Project_Implementation/annclassification/app.py
+++ ANN_Project_Implementation/annclassification/app.py
@@ -11,7 +11,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "model.h5")
 
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model(MODEL_PATH)
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
